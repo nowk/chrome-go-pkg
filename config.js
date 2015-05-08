a71a39d6262712560ec0f4c7c2f3b52fd78a9c51
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-05-07 23:05:19 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Go Pkg",
+   "homepage": "http://golang.org/pkg",
+   "enableNavBttns": true,
+   "enableHomeBttn": true,
+   "enableReloadBttn": true,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
